1cbd99e8086a6c4e58cc43be5f84ebe4fa43123f
Adonjobiii <adonjobi2003@gmail.com>
2026-03-16 13:17:42 +0530

    @

--- app.py
+++ app.py
@@ -296,4 +296,6 @@ def get_collection():
     return {"success": True, "items": user_collection}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
